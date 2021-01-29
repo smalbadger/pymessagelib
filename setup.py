b807a2452ecae834fb58b3921d506ff8961134c3
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2021-01-29 07:56:23 -0700

    adding setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='pymessagelib',
-    version='0.0.0',
-    packages=['pymessagelib'],
-    url='',
-    license='',
-    author='smalbadger',
-    author_email='smalbadger@gmail.com',
-    description='A simple library for constructing, dissecting, and comparing specially formatted data'
+    name="pymessagelib",
+    version="0.0.0",
+    packages=["pymessagelib"],
+    url="",
+    license="",
+    author="smalbadger",
+    author_email="smalbadger@gmail.com",
+    description="A simple library for constructing, dissecting, and comparing specially formatted data",
 )
