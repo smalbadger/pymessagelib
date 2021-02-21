504a74d32bf2a769601495539f6f4b5723ea428c
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2021-02-21 01:07:05 -0700

    Test repr of field object without value
    A field will only not have a value if it was used as a format specifier
    in a message format definition.

---
+++ test/test_message_formats.py
@@ -0,0 +1,11 @@
+import unittest
+from message_builder import MessageBuilder
+from msg_definitions import msg_fmts, register_defs
+
+
+class TestMessageFormats(unittest.TestCase):
+    def testFormatFieldRepr(self):
+        builder = MessageBuilder()
+        OUTPUTS = builder.build_message_class("OUTPUTS", register_defs["OUTPUTS"])
+        
+        self.assertTrue("Byte" in repr(OUTPUTS.format['cautions']))
