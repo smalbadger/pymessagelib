0c67e3a9a6b72165e6fa17aa39aa49cd53cc1120
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2021-02-24 18:14:27 -0700

    fix codefactor issues

--- pymessagelib/dependency_graph.py
+++ pymessagelib/dependency_graph.py
@@ -33,11 +33,11 @@ class DependencyGraph:
 
             # Recur for all neighbors if any neighbor is visited and in recStack then graph is cyclic
             for neighbor in self.graph[v]:
-                if visited[neighbor] == False:
-                    if isCyclicUtil(neighbor, visited, recStack) == True:
+                if not visited[neighbor]:
+                    if isCyclicUtil(neighbor, visited, recStack):
                         self._cycle = [node for node in recStack]
                         return True
-                elif recStack[neighbor] == True:
+                elif recStack[neighbor]:
                     return True
 
             # The node needs to be popped from recursion stack before function ends
@@ -47,8 +47,8 @@ class DependencyGraph:
         visited = {i: False for i in self.graph.keys()}
         recStack = {i: False for i in self.graph.keys()}
         for node in self.graph.keys():
-            if visited[node] == False:
-                if isCyclicUtil(node, visited, recStack) == True:
+            if not visited[node]:
+                if isCyclicUtil(node, visited, recStack):
                     return True
         return False
     
