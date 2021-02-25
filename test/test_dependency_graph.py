4b69dd161d3c991b9b0fd3491d86d5038c6c49ad
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2021-02-25 12:31:22 -0700

    split up dependency graph tests and added another to get better coverage

--- test/test_dependency_graph.py
+++ test/test_dependency_graph.py
@@ -15,9 +15,23 @@ class TestDependencyGraph(unittest.TestCase):
         self.graph.addEdge("world", "cool")
         self.graph.addEdge("cool", "hello")
         self.assertTrue(self.graph.isCyclic())
+        
+    def testCycle(self):
+        self.graph.addEdge("hello", "world")
+        self.graph.addEdge("world", "cool")
+        self.graph.addEdge("cool", "hello")
         self.assertEqual(self.graph.cycle, ["hello", "world", "cool"])
 
     def testNotCyclic(self):
         self.graph.addEdge("hello", "world")
         self.graph.addEdge("world", "cool")
         self.assertFalse(self.graph.isCyclic())
+
+    def testNumVertices(self):
+        self.assertEqual(self.graph.num_vertices, 0)
+        self.graph.addEdge("hello", "world")
+        self.assertEqual(self.graph.num_vertices, 2)
+        self.graph.addEdge("world", "cool")
+        self.assertEqual(self.graph.num_vertices, 3)
+        self.graph.addEdge("cool", "hello")
+        self.assertEqual(self.graph.num_vertices, 3)
