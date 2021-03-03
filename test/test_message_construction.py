2c587991297704f44b9efa690497e073acd0e622
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2021-03-02 19:33:58 -0700

    separated field name test into different method

--- test/test_message_construction.py
+++ test/test_message_construction.py
@@ -12,6 +12,11 @@ class TestMessageConstruction(unittest.TestCase):
         self.assertTrue(outputs.reset2 == "b1")
         self.assertTrue(outputs.cautions == "x80")
         self.assertTrue(outputs.unused == "x0")
+        
+    def testFieldNames(self):
+        builder = MessageBuilder()
+        OUTPUTS = builder.build_message_class("OUTPUTS", register_defs["OUTPUTS"])
+        outputs = OUTPUTS.from_data("b11100000000000000000000000000000")
         self.assertEqual(outputs.reset1.name, "reset1")
         self.assertEqual(outputs.reset2.name, "reset2")
         self.assertEqual(outputs.cautions.name, "cautions")
