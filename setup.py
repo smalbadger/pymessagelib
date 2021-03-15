c3d63cc167b22134eb8ad254477a1b9b9bc3b732
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2021-03-14 20:12:27 -0700

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pymessagelib",
-    version="0.1.1",
+    version="0.1.2",
     description="Give structure to hexadecimal messages.",
     long_description=README,
     long_description_content_type="text/markdown",
