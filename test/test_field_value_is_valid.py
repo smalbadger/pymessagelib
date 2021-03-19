b0fc09b44bde3150a7be2e397caa365c18731d70
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2021-03-18 23:29:19 -0700

    determine if a string contains any invalid values

--- test/test_field_value_is_valid.py
+++ test/test_field_value_is_valid.py
@@ -1,11 +1,23 @@
 import unittest
-from pymessagelib import MessageBuilder, InvalidFieldDataException, CircularDependencyException
+from pymessagelib import MessageBuilder, Field
 from msg_definitions import msg_fmts, register_defs, circular_dep
 
 
 class TestFieldValueIsValid(unittest.TestCase):
     def setUp(self):
         self.builder = MessageBuilder(definitions=msg_fmts)
+
+    def testValidChars_Hex(self):
+        self.assertEqual("".join(Field.get_valid_chars(Field.Format.Hex)), "0123456789abcdefABCDEF")
+
+    def testValidChars_Dec(self):
+        self.assertEqual("".join(Field.get_valid_chars(Field.Format.Dec)), "0123456789")
+
+    def testValidChars_Oct(self):
+        self.assertEqual("".join(Field.get_valid_chars(Field.Format.Oct)), "01234567")
+
+    def testValidChars_Bin(self):
+        self.assertEqual("".join(Field.get_valid_chars(Field.Format.Bin)), "01")
 
     def testNoFormatSpecifier(self):
         msg1 = self.builder.GET_ADDR(ptr="x00000054", addr="b10001101001")
