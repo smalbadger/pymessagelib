d15d53c5bbe5c9f564fc56bb0b48204ea8488cfd
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2021-03-18 22:42:57 -0700

    check for invalid types in value_is_valid

--- test/test_field_value_is_valid.py
+++ test/test_field_value_is_valid.py
@@ -10,3 +10,11 @@ class TestFieldValueIsValid(unittest.TestCase):
     def testNoFormatSpecifier(self):
         msg1 = self.builder.GET_ADDR(ptr="x00000054", addr="b10001101001")
         self.assertEqual(msg1.ptr.value_is_valid("00000054"), False)
+
+    def testInvalidType(self):
+        msg1 = self.builder.GET_ADDR(ptr="x00000054", addr="b10001101001")
+        self.assertEqual(msg1.ptr.value_is_valid(False), False)
+
+    def testInvalidValue(self):
+        msg1 = self.builder.GET_ADDR(ptr="x00000054", addr="b10001101001")
+        self.assertEqual(msg1.ptr.value_is_valid("xfjskjfjfj"), False)
